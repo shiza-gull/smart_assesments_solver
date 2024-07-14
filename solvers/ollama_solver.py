4838a2bf6407c5a849b7538530cc018181bcf7c0
Muhammad Hussain <sdmhmmdhussain@gmail.com>
2024-07-14 16:55:44 +0500

    add option to use custom models

--- solvers/ollama_solver.py
+++ solvers/ollama_solver.py
@@ -3,9 +3,9 @@ import ollama
 SYSTEM_PROMPT = "You will get a quiz to solve. You will output just the name of the correct option like A, B, C. If no option matches the correct answer choose something randomly."
 
 
-def solve_quiz(question):
+def solve_quiz(question, model="llama3"):
     answer = ollama.chat(
-        model="llama3",
+        model=model,
         messages=[
             {"role": "system", "content": SYSTEM_PROMPT},
             {"role": "user", "content": question},
